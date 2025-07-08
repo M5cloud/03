c68d9b9eb1e456c15c8b193a4d955bad75486942
m5cloud <muhammadyousaf0263706@gmail.com>
2025-07-08 11:36:13 +0500

    hgfdsdfgh

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,35 @@ function App() {
   
   return (
     <>
-      <div style={{backgroundColor: 'red', height:100, width:100}}>
+      <div style={{backgroundColor: 'red', height:500, width:500}}>
+         <br />
+         <h2>Itx Abdullah Software Engineer</h2>
+         <br />
          <b>Hello</b>
-         <button>click me </button>
+         <br />
+         
+         <button style={{backgroundColor:'yellow'}}>click me </button>
+         <div style={{display:'flex', justifyContent:'center', }}>
+         <div style={{backgroundColor: 'purple', height:100, width:100, margin:20, display:'flex'}}></div>
+         <div style={{backgroundColor: 'white', height:100, width:100, margin:20, display:'flex'}}></div>
+         <div style={{backgroundColor: 'gray', height:100, width:100, margin:20, display:'flex'}}></div>
+         <br />
+         
+
+         </div>
+         <div style={{display:'flex', justifyContent:'center', }}>
+         <div style={{backgroundColor: 'pink', height:100, width:100, margin:20, display:'flex'}}></div>
+         <div style={{backgroundColor: 'green', height:100, width:100, margin:20, display:'flex'}}></div>
+         <div style={{backgroundColor: 'blue', height:100, width:100, margin:20, display:'flex'}}></div>
+         <br />
+         
+
+         </div>
+        
+         
+      
       </div>
+
      
     </>
   )
