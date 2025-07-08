3fc26482c9480a609fd496bbeecc951a8d5df336
m5cloud <muhammadyousaf0263706@gmail.com>
2025-07-08 10:41:32 +0500

    first and main commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div style={{backgroundColor: 'red', height:100, width:100}}>
+         <b>Hello</b>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
